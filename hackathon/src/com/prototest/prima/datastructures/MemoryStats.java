5f63054b71c460ef7bfd20ada675b141e66ddc94
msiwiec <msiwiec@yahoo.com>
2014-01-15 13:21:20 -0700

    fix build for prima app context

--- hackathon/src/com/prototest/prima/datastructures/MemoryStats.java
+++ hackathon/src/com/prototest/prima/datastructures/MemoryStats.java
@@ -1,38 +1,34 @@
 package com.prototest.prima.datastructures;
 
-import com.prototest.prima.PRIMA;
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.ActivityManager;
-import android.os.Build;
 
- public class MemoryStats implements SystemStats {
-	public long current;
-	public long max;
-	public long available;
-	
-	public MemoryStats()
-	{
+import com.prototest.prima.PrimaApp;
 
-	}
-	
-	public double getPercentage()
-	{
-		double percent = 100*((double)this.current/(double)this.max);
-	return percent;	
-	}
-	
-	public void GetStats()
-	{
-		
-		ProcessBuilder cmd;
-		StringBuffer strMemory = new StringBuffer();
-		ActivityManager actvityManager = (ActivityManager) PRIMA.getAppContext().getSystemService(android.content.Context.ACTIVITY_SERVICE );
-		ActivityManager.MemoryInfo mInfo = new ActivityManager.MemoryInfo ();
-		actvityManager.getMemoryInfo( mInfo );
-		this.available = mInfo.availMem/1048576L;
-		//this.max = mInfo.totalMem/1048576L;
-		this.current = this.max - this.available;
-	}
+public class MemoryStats implements SystemStats {
+   public long current;
+   public long max;
+   public long available;
+
+   public MemoryStats() {
+
+   }
+
+   public double getPercentage() {
+      double percent = 100 * ((double) this.current / (double) this.max);
+      return percent;
+   }
+
+   public void GetStats() {
+
+      ProcessBuilder cmd;
+      StringBuffer strMemory = new StringBuffer();
+      ActivityManager actvityManager = (ActivityManager) PrimaApp.getAppContext().getSystemService(
+            android.content.Context.ACTIVITY_SERVICE);
+      ActivityManager.MemoryInfo mInfo = new ActivityManager.MemoryInfo();
+      actvityManager.getMemoryInfo(mInfo);
+      this.available = mInfo.availMem / 1048576L;
+      // this.max = mInfo.totalMem/1048576L;
+      this.current = this.max - this.available;
+   }
 
 }

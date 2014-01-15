c72eb269fc0f24413897e1ac0362d8e6d6a426c8
msiwiec <msiwiec@yahoo.com>
2014-01-15 13:33:50 -0700

    add logging to main activity

--- hackathon/src/com/prototest/prima/MainActivity.java
+++ hackathon/src/com/prototest/prima/MainActivity.java
@@ -1,29 +1,35 @@
 package com.prototest.prima;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+   static final String TAG = "MainActivity";
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	public void switchToGraph(View view) {
-		Intent intent = new Intent(MainActivity.this, SimpleXYPlotActivity.class);
-	    startActivity(intent);
-	}
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Log.d(TAG, "onCreate()");
+      setContentView(R.layout.activity_main);
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      Log.d(TAG, "onCreateOptionsMenu()");
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
+
+   public void switchToGraph(View view) {
+      Log.d(TAG, "switchToGraph()");
+      Intent intent = new Intent(MainActivity.this, SimpleXYPlotActivity.class);
+      startActivity(intent);
+   }
 
 }

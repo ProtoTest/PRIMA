a026e4d7a24da3c1b8fe16ba1a50ebb486c62bf8
msiwiec <msiwiec@yahoo.com>
2014-01-15 12:21:08 -0700

    seed the db. Fix db create for batt stats table

--- hackathon/src/com/prototest/prima/PrimaApp.java
+++ hackathon/src/com/prototest/prima/PrimaApp.java
@@ -1,11 +1,17 @@
 package com.prototest.prima;
 
 import android.app.Application;
+import android.content.ContentValues;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Configuration;
 import android.preference.PreferenceManager;
 import android.util.Log;
+
+import com.prototest.prima.contentprovider.PrimaContentProvider;
+import com.prototest.prima.database.BATTStatsTable;
+import com.prototest.prima.database.CPUStatsTable;
+import com.prototest.prima.database.MEMStatsTable;
 
 public class PrimaApp extends Application implements OnSharedPreferenceChangeListener {
 
@@ -81,7 +87,48 @@ public class PrimaApp extends Application implements OnSharedPreferenceChangeLis
 
    private void seedDatabase() {
       Log.d(TAG, "seedDatabase()");
-      // getContentResolver().insert(url, values);
+      ContentValues batt_values = new ContentValues();
+      ContentValues mem_values = new ContentValues();
+      ContentValues cpu_values = new ContentValues();
 
+      int level = 100;
+      int scale = 50;
+      int temp = 120;
+      int voltage = 20;
+
+      int free = 95;
+      int used = 5;
+      double percent_used = 50.0;
+
+      for (int i = 1; i <= 50; i++) {
+         // battery stats
+         batt_values.put(BATTStatsTable.COLUMN_LEVEL, (level % i) * Math.random());
+         batt_values.put(BATTStatsTable.COLUMN_SCALE, scale % i);
+         batt_values.put(BATTStatsTable.COLUMN_TEMP, temp += 1);
+         batt_values.put(BATTStatsTable.COLUMN_VOLTAGE, (voltage % i) * Math.random());
+         getContentResolver().insert(PrimaContentProvider.CONTENT_URI_BATT, batt_values);
+
+         // cpu status
+         if (i % 2 == 0) {
+            cpu_values.put(CPUStatsTable.COLUMN_FREE, free -= 1);
+            cpu_values.put(CPUStatsTable.COLUMN_PERCENT_USED, percent_used += 1);
+            cpu_values.put(CPUStatsTable.COLUMN_USED, used += 1);
+         } else {
+            cpu_values.put(CPUStatsTable.COLUMN_FREE, free += 1);
+            cpu_values.put(CPUStatsTable.COLUMN_PERCENT_USED, percent_used -= 1);
+            cpu_values.put(CPUStatsTable.COLUMN_USED, used -= 1);
+         }
+
+         getContentResolver().insert(PrimaContentProvider.CONTENT_URI_CPU, cpu_values);
+
+         // mem status
+         mem_values.put(MEMStatsTable.COLUMN_AVAILABLE, 7898);
+         mem_values.put(MEMStatsTable.COLUMN_CURRENT, 3333);
+         mem_values.put(MEMStatsTable.COLUMN_MAX, 8192);
+         getContentResolver().insert(PrimaContentProvider.CONTENT_URI_MEM, mem_values);
+      }
+
+      prefs.edit().putBoolean("databaseSeeded", true).commit();
+      Log.d(TAG, "Database has been successfully seeded!!!");
    }
 }

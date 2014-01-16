7775b68b36ed0be8a0a28305d74d146f50f8c57b
mponce1 <mponce@prototest.com>
2014-01-16 13:01:29 -0700

    fixed timer

--- hackathon/src/com/prototest/prima/MainActivity.java
+++ hackathon/src/com/prototest/prima/MainActivity.java
@@ -1,20 +1,27 @@
 package com.prototest.prima;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+<<<<<<< HEAD
 
 import com.prototest.prima.contentprovider.PrimaContentProvider;
 import com.prototest.prima.datastructures.BatteryStats;
 import com.prototest.prima.datastructures.MemoryStats;
 import com.prototest.prima.datastructures.ProcessorStats;
 import com.prototest.prima.monitor.SystemMonitor;
+=======
+>>>>>>> Timer starts/stops when pressed
 
 public class MainActivity extends Activity {
 
@@ -23,13 +30,22 @@ public class MainActivity extends Activity {
    private static SystemMonitor memoryMonitor;
    private static SystemMonitor processorMonitor;
    private static boolean recording = false;
+<<<<<<< HEAD
    private TextView recordingLabel;
 
+=======
+   private static Timer timer;
+   private int elapsedTime = 0;
+   private TextView stopwatchText;
+
+   
+>>>>>>> Timer starts/stops when pressed
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       Log.d(TAG, "onCreate()");
       setContentView(R.layout.activity_main);
+      stopwatchText = (TextView) findViewById(R.id.stopwatch);
       batteryMonitor = new SystemMonitor(new BatteryStats());
       memoryMonitor = new SystemMonitor(new MemoryStats());
       try {
@@ -74,6 +90,7 @@ public class MainActivity extends Activity {
       processorMonitor.StartMonitoring();
       recordingLabel.setText(R.string.recording);
    }
+<<<<<<< HEAD
 
    private void stopRecording(View view) {
       Log.d(TAG, "stopRecording");
@@ -83,4 +100,60 @@ public class MainActivity extends Activity {
       recordingLabel.setText(R.string.not_recording);
       switchToGraph(view);
    }
+=======
+   
+   private void startRecording()
+   {
+	   Log.d(TAG,"startRecording");
+	   startTimer();
+	   batteryMonitor.StartMonitoring();
+	   memoryMonitor.StartMonitoring();
+	   processorMonitor.StartMonitoring();
+   }
+   
+   private void stopRecording()
+   {
+	   Log.d(TAG,"stopRecording");
+	   stopTimer();
+	   batteryMonitor.StopMonitoring();
+	   memoryMonitor.StopMonitoring();
+	   processorMonitor.StopMonitoring();
+   }
+   
+   private void stopTimer() {
+	timer.cancel();
+	timer = null;
+	elapsedTime = 0;
 }
+
+private void startTimer() {
+	    timer = new Timer();
+	    timer.scheduleAtFixedRate(new TimerTask() {
+
+
+			public void run() {
+	            elapsedTime += 1; //increase every sec
+	            mHandler.obtainMessage(1).sendToTarget();
+
+	        }
+	    }, 0, 1000);
+	}
+
+	private Handler mHandler = new Handler() {
+	    public void handleMessage(Message msg) {
+	        stopwatchText.setText(formatStopWatchTime(elapsedTime)); //this is the textview
+	    }
+	};
+	
+	private String formatStopWatchTime(int elapsedSeconds) {
+		String timeText;
+		int hours = elapsedSeconds / 3600;
+		int minutes = elapsedSeconds / 60;
+		int seconds = elapsedSeconds / 1;
+		int displayMinutes = minutes % 60;
+		int displaySeconds = seconds % 60;
+		return String.format("%02d:%02d:%02d", hours, displayMinutes, displaySeconds);
+	}
+>>>>>>> Timer starts/stops when pressed
+}
+
